7a2816fc583e28a068a507530c717a49a0547dd7
Ibrahim Abdelazim <ibrahimabdelazim@yahoo.com>
2022-03-27 23:43:15 +0200

    Edit Me Fix

--- controllers/meController.js
+++ controllers/meController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const Vendor = require('../models/vendorModel');
 const AppError = require('../utils/appError');
 
 exports.getMyData = async (req, res, next) => {
@@ -19,17 +20,27 @@ exports.getMyData = async (req, res, next) => {
 exports.editMe = async (req, res, next) => {
   try {
     const { name, photo, address, phone } = req.body;
-    const updatedUser = await User.findByIdAndUpdate(req.user._id, {
-      name,
-      photo,
-      address,
-      phone,
-    });
+    let updatedPerson;
+    if (req.user.role === 'admin' || req.user.role === 'user') {
+      updatedPerson = await User.findByIdAndUpdate(req.user._id, {
+        name,
+        photo,
+        address,
+        phone,
+      });
+    } else if (req.user.role === 'vendor') {
+      updatedPerson = await Vendor.findByIdAndUpdate(req.user._id, {
+        name,
+        photo,
+        address,
+        phone,
+      });
+    }
     res.status(201).json({
       status: 'success',
       data: {
         status: 'Your data is updated',
-        updatedUser,
+        updatedUser: updatedPerson,
       },
     });
   } catch (err) {
